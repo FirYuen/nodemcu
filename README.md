b6217872ac90bb3e1e1e856b698c10e32afd3a1a
bin仔 <442416294@qq.com>
2018-02-02 11:41:35 +0800

    修改文档

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-dht11.lua 读取温度湿度
-leds.lua RGB三色LED呼吸灯
+文档:https://www.jianshu.com/u/98ad5cd8ac35
+- 
+- dht11.lua 读取温度湿度
+- leds.lua RGB三色LED呼吸灯
--- dht11.lua
+++ dht11.lua
@@ -1,7 +1,7 @@
 function task()
-    local pin = 4
-    local status,temp,humi=dht.read(pin)
-    local str = string.format("当前温度:%f 湿度:%f", temp, humi)
+    local _,temp11,humi11=dht.read11(5)
+    local _,temp22,humi22=dht.read(4)
+    local str = string.format("温度:%.1f/%.1f 湿度:%.1f/%.1f", temp11, temp22, humi11, humi22)
     print(str)
 end
 tmr.create():alarm(5000, tmr.ALARM_AUTO, task)
